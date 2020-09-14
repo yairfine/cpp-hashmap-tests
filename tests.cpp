41653ab54cb91d702e6186b866e51215bdc85237
yairfine <yair.yfs@gmail.com>
2020-09-14 12:03:26 +0300

    test ctor 1

--- tests.cpp
+++ tests.cpp
@@ -30,6 +30,80 @@ void testDefaultConstruct()
     std::cout << "PASS - testDefaultConstruct" << std::endl;
 }
 
+void testConstruct1()
+{
+    std::vector<KeyT1> keys = {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
+    std::vector<ValueT1> values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    HashMap<KeyT1, ValueT1> map(keys.begin(), keys.end(), values.begin(), values.end());
+
+    assert(map.getSize() == 9);
+    assert(map.at("a") == 1);
+    assert(map.at("b") == 2);
+    assert(map.at("c") == 3);
+    assert(map.at("d") == 4);
+    assert(map.at("e") == 5);
+    assert(map.at("f") == 6);
+    assert(map.at("g") == 7);
+    assert(map.at("h") == 8);
+    assert(map.at("i") == 9);
+
+
+    std::vector<KeyT1> keys2 = {"a", "b", "c", "d", "e", "f", "g", "a", "b", "h", "i"};
+    std::vector<ValueT1> values2 = {1, 2, 3, 4, 5, 6, 7, 100, 200, 8, 9};
+
+    HashMap<KeyT1, ValueT1> map2(keys2.begin(), keys2.end(), values2.begin(), values2.end());
+
+    assert(map.getSize() == 9);
+    assert(map2.at("a") == 100);
+    assert(map2.at("b") == 200);
+    assert(map2.at("c") == 3);
+    assert(map2.at("d") == 4);
+    assert(map2.at("e") == 5);
+    assert(map2.at("f") == 6);
+    assert(map2.at("g") == 7);
+    assert(map2.at("h") == 8);
+    assert(map2.at("i") == 9);
+
+    try
+    {
+        auto it = keys.begin();
+        it++;
+        HashMap<KeyT1, ValueT1> map(it, keys.end(), values.begin(), values.end());
+        assert(!"Constructor1 should throw an exception if iterators are not at the same length");
+    }
+    catch (std::exception &e) {}
+
+    try
+    {
+        auto it = keys.end();
+        it--;
+        HashMap<KeyT1, ValueT1> map(keys.begin(), it, values.begin(), values.end());
+        assert(!"Constructor1 should throw an exception if iterators are not at the same length");
+    }
+    catch (std::exception &e) {}
+
+    try
+    {
+        auto it = values.begin();
+        it++;
+        HashMap<KeyT1, ValueT1> map(keys.begin(), keys.end(), it, values.end());
+        assert(!"Constructor1 should throw an exception if iterators are not at the same length");
+    }
+    catch (std::exception &e) {}
+
+    try
+    {
+        auto it = values.end();
+        it--;
+        HashMap<KeyT1, ValueT1> map(keys.begin(), keys.end(), values.begin(), it);
+        assert(!"Constructor1 should throw an exception if iterators are not at the same length");
+    }
+    catch (std::exception &e) {}
+
+    std::cout << "PASS - testConstruct1" << std::endl;
+}
+
 
 
 int main()
@@ -37,6 +111,7 @@ int main()
     std::cout << "~~~~~ Starting tests ~~~~~" << std::endl;
 
     testDefaultConstruct();
+    testConstruct1();
 
 //
 //
