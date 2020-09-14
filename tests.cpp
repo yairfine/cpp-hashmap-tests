14e7ef787e3a44d408eaef7f073a07306712ef80
yairfine <yair.yfs@gmail.com>
2020-09-14 10:32:41 +0300

    added first test

--- tests.cpp
+++ tests.cpp
@@ -4,25 +4,52 @@
 #include <iostream>
 #include "HashMap.hpp"
 #include <cassert>
-
 #include <vector>
+
+#define KeyT1 std::string
+#define ValueT1 int
+
+#define INITIAL_CAPACITY 16
+#define INITIAL_SIZE 0
+
+void testDefaultConstruct()
+{
+    HashMap<KeyT1, ValueT1> map;
+
+    assert(map.getSize() == INITIAL_SIZE);
+    assert(map.getCapacity() == INITIAL_CAPACITY);
+    assert(map.empty() == true);
+
+    map.insert("a", 1);
+
+    assert(map.getSize() == 1);
+    assert(map.getCapacity() == INITIAL_CAPACITY);
+    assert(map.empty() == false);
+
+
+    std::cout << "PASS - testDefaultConstruct" << std::endl;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "~~~~~ Starting tests ~~~~~" << std::endl;
 
+    testDefaultConstruct();
 
-
-    std::vector<std::string> names = {"a", "b", "c", "d", "e", "f", "g", "a", "b"};
-    std::vector<int> grades = {1, 2, 3, 4, 5, 6, 7, 100, 200};
-
-    HashMap<std::string, int> map1(names.begin(), names.end(), grades.begin(), grades.end());
-
-    map1.print();
-
-    std::cout << map1.bucket_index("a") << std::endl;
-    std::cout << map1.bucket_index("b") << std::endl;
-    std::cout << map1.bucket_index("c") << std::endl;
+//
+//
+//    std::vector<std::string> names = {"a", "b", "c", "d", "e", "f", "g", "a", "b"};
+//    std::vector<int> grades = {1, 2, 3, 4, 5, 6, 7, 100, 200};
+//
+//    HashMap<std::string, int> map1(names.begin(), names.end(), grades.begin(), grades.end());
+//
+//    map1.print();
+//
+//    std::cout << map1.bucket_index("a") << std::endl;
+//    std::cout << map1.bucket_index("b") << std::endl;
+//    std::cout << map1.bucket_index("c") << std::endl;
 
 
 
