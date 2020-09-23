7ab67510b800530ba5d738cca2cb5d634660b699
yair fine <yair.yfs@gmail.com>
2020-09-23 18:58:32 +0300

    added tests for copy ctor and operator=

--- tests.cpp
+++ tests.cpp
@@ -333,6 +333,10 @@ void testOperatorAssignment()
     assert(map1.at("m") == 13);
 
 
+    // for Valgrind check:
+    map1 = map1;
+
+
     #ifndef VAL
     myProgressBar.addToOutputMsg("PASS - testOperatorAssignment");
     myProgressBar++;
